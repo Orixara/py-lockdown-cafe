a0c919556a685b42638cdff5aefd10c52f761bd8
Oleksandr Popov <orbik.rumion22@gmail.com>
2025-07-06 18:47:38 +0300

    added conversion of string to object datetime

--- app/cafe.py
+++ app/cafe.py
@@ -14,7 +14,15 @@ class Cafe:
     def visit_cafe(self, visitor: dict) -> str:
         if "vaccine" not in visitor:
             raise NotVaccinatedError("Visitor is not vaccinated")
-        if visitor["vaccine"]["expiration_date"] < datetime.date.today():
+
+        expiration_date = visitor["vaccine"]["expiration_date"]
+        if isinstance(expiration_date, str):
+            expiration_date = datetime.datetime.strptime(
+                expiration_date,
+                "%Y-%m-%d"
+            ).date()
+
+        if expiration_date < datetime.date.today():
             raise OutdatedVaccineError("Visitor has outdated vaccine")
         if "wearing_a_mask" not in visitor or not visitor["wearing_a_mask"]:
             raise NotWearingMaskError("Visitor doesn't have a mask")
